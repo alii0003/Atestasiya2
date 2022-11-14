3b979588cfabae6e83f75436999c9f342b500c89
alii0003 <89392340+alii0003@users.noreply.github.com>
2022-11-14 18:49:50 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function newtime(){
+    let time = new Date();
+    return `${time.getHours()}:${time.getMinutes}`
+}
+class Message{
+    constructor(ad,soyad,metin){
+this.ad = Ad;
+this.vaxt = Vaxt;
+this.metin = metin
+    }
+}
+toString(){
+    return this.time + '' + this.name + 
+}
